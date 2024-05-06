decce6a43ea5e4edf8ac18a8a2ebed6948fc491a
rittin2803 <rittin2803@gmail.com>
2024-05-06 11:28:12 +0530

    Database Connected

---
+++ config/dbconfig.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const hotelRouter = require("./routes/hotel.router");
+const connectDB = require("./config/dbconfig")
 
 const app = express();
 
 app.use(express.json());
+connectDB();
 
 const PORT = 3000;
 
@@ -14,6 +17,9 @@ app.get("/", (req, res)=>{
 
 app.use("/api/v1/hotels", hotelRouter);
 
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server is running at ${PORT}`);
+mongoose.connection.once("open", ()=>{
+    console.log("Connected to Database");
+    app.listen(process.env.PORT || PORT, ()=>{
+        console.log(`Server is running at ${PORT}`);
+    });
 });
